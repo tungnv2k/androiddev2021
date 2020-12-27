9819542678b8bdff46183c0ea7b1add792cfc746
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-12-27 12:47:42 +0700

    Practical work 11

--- src/main/java/vn/edu/usth/weather/activity/WeatherActivity.java
+++ src/main/java/vn/edu/usth/weather/activity/WeatherActivity.java
@@ -2,8 +2,10 @@ package vn.edu.usth.weather.activity;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import androidx.annotation.Nullable;
@@ -13,6 +15,7 @@ import androidx.viewpager.widget.ViewPager;
 import vn.edu.usth.weather.R;
 import vn.edu.usth.weather.adapter.HomeFragmentPagerAdapter;
 
+import java.io.*;
 import java.util.Locale;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -22,14 +25,49 @@ public class WeatherActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate: called");
-		setApplicationLocale("vi");
 		setContentView(R.layout.activity_main);
+
+		handleSoundFile();
+		playSoundFile();
 
 		PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager(), 1);
 		ViewPager pager = findViewById(R.id.pager);
 		pager.setOffscreenPageLimit(3);
 		pager.setAdapter(adapter);
 		pager.setCurrentItem(1);
+	}
+
+	private void playSoundFile() {
+		MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.mp3_sound);
+		mediaPlayer.start();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			mediaPlayer.stop();
+		}
+	}
+
+	private void handleSoundFile() {
+		File sdCard = Environment.getExternalStorageDirectory();
+		File dir = new File(sdCard.getAbsolutePath() + "/tempdev");
+		dir.mkdirs();
+		File file = new File(dir, "mp3_sound.mp3");
+
+		try (InputStream mp3Input = this.getResources().openRawResource(R.raw.mp3_sound);
+		     OutputStream sdCardOutput = new FileOutputStream(file)) {
+			byte[] buffer = new byte[4 * 1024];
+			int read;
+
+			while ((read = mp3Input.read(buffer)) != -1) {
+				sdCardOutput.write(buffer, 0, read);
+			}
+
+			sdCardOutput.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@SuppressWarnings("deprecated")
